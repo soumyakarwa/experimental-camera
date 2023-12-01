d09e98a59356605c5a00b3841d732a7b8cfa8775
soumyakarwa <soumyakarwa01@gmail.com>
2023-11-30 22:03:05 -0500

    whatever

--- sketch.js
+++ sketch.js
@@ -1,7 +1,51 @@
+var vid; 
+var startPos = {x: 10, y: 10}; 
+var increment = 20;
+var pixelIndices = {x: [], y: []}; 
+var rVal = 0; 
+var gVal = 0; 
+var bVal = 0;
+
+// BUILT ON THE IN-CLASS CODE FOUND HERE: 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  vid = createCapture(VIDEO); 
+  vid.size(windowWidth, windowHeight);
+  vid.hide();
+  for (let i=startPos.x; i < vid.width; i+=increment) {
+    pixelIndices.x.push(i); 
+    pixelIndices.y.push(i); 
+  }
+  pixelIndices.x = shuffle(pixelIndices.x); 
+  pixelIndices.y = shuffle(pixelIndices.y); 
 }
 
 function draw() {
-  background(220);
+  background(0);
+  vid.loadPixels();
+  let colors = []; 
+  for (let i=startPos.x; i < vid.width; i+=increment) {
+    for (let j=startPos.y; j < vid.height; j+=increment) {
+      let pindex = (i + (j * vid.width))* 4;
+      let r = vid.pixels[pindex + 0] + rVal;
+      let g = vid.pixels[pindex + 1] + gVal;
+      let b = vid.pixels[pindex + 2] + bVal;
+      fill(r, g, b); 
+      ellipse(i, j, 20, 20); 
+    }
+  }
+}
+
+function keyTyped(){
+  switch(key){
+    case("r"):
+    rVal+=10; 
+    break; 
+    case("g"):
+    gVal+=10; 
+    break; 
+    case("b"):
+    bVal+=10; 
+    break; 
+  }
 }
